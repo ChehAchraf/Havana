72e3f1b2f2756fe4b7ad2110a85af4bf536f77e9
chehachraf <achrafchehboun@outlook.com>
2026-01-07 12:15:01 +0100

    improving the template

---
+++ src/app/shared/components/ui/search-input/search-input.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-search-input',
+  imports: [NgIcon],
+  templateUrl: './search-input.html',
+  styleUrl: './search-input.css',
+})
+export class SearchInput {
+
+}
